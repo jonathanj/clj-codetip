b4004fee3d470c65f50d3fbd469b2c340b002bfa
Tristan Seligmann <mithrandi@mithrandi.net>
2015-06-08 15:53:45 +0200

    Stuff.

--- profiles.clj
+++ profiles.clj
@@ -1,2 +1,2 @@
-{:prod {:joplin {:databases {:sql-prod {:type :jdbc :url "jdbc:sqlite:/srv/codetip/db/codetip.db"}}}}
- :uberjar {:uberjar-name "codetip.jar"}}
+{:prod    {:joplin {:databases {:sql-prod {:type :jdbc :url "jdbc:sqlite:target/codetip.db"}}}}
+ :uberjar {:uberjar-name "clj-codetip.jar"}}
